f5d03e8da0d1db3058ce68961e09af07284cc580
AdnanRohmatKurniansah <atnandimas@gmail.com>
2023-03-24 20:50:36 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.main')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-4 bg-primary">
+            <h1>ini home</h1>
+        </div>
+        <div class="col-md-8 bg-danger">
+            <h1>sidebar</h1>
+        </div>
+    </div>
+@endsection
