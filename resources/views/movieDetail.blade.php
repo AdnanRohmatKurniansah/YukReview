67d497f3b5aee310f6ef5bea2af429f59cafcc30
AdnanRohmatKurniansah <atnandimas@gmail.com>
2023-03-28 23:07:28 +0700

    make moviesDetail

--- resources/views/movieDetail.blade.php
+++ resources/views/movieDetail.blade.php
@@ -1,9 +1,153 @@
 @extends('layout.main')
 
 @section('content')
-    <div class="col-md-10">
-        <video width="400px" height="350px" controls>
-            <source src="https://youtu.be/TcMBFSGVi1c" type="video/mp4">
-        </video>
+<section id="movieDetail"  style="background-image: url('/assets/img/background/bg-1.png'); background-repeat: no-repeat; background-size: cover; color: #fff">
+    <div class="container">
+        <div class="trailer col-12 pt-5">
+            <iframe class="d-flex justify-content-center mt-5 mb-3" width="100%" height="400" src="https://www.youtube.com/embed/TcMBFSGVi1c">
+            </iframe>
+        </div>
+        <div class="row mt-5">
+            <!-- left -->
+            <div class="col-lg-8">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img src="/assets/img/avenger.jpg" class="card-img" alt="..." style="min-height: 300px">
+                          </div>
+                    </div>
+                    <div class="col-md-8">
+                        <h3 class="movie-title">Avenger End Game</h3>
+                        <div class="movie-info">
+                            <p>Duration: <i class="bi bi-stopwatch"></i> 02 hours 30 minutes</p>
+                            <ul class="list-unstyled">
+                                <li><b>Director : </b> </li>
+                                <li><b>Genre :</b> Cartoon, Sci-fi</li>
+                                <li><b>Release :</b> 24 April 2019</li>
+                                <li><b>Language :</b> English</li>
+                                <li><b>Rating :</b> 8</li>
+                            </ul>
+                        </div>
+    
+                    </div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-12">
+                        <h3 style="border-bottom: 1px solid rgb(212, 212, 212); padding-bottom: 5px; ">Synopsis</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos doloremque commodi earum. Reiciendis, rerum mollitia. Dolorum, atque, dolore ipsum illo adipisci doloribus debitis, nostrum explicabo nobis sequi fugiat ipsa laborum iure quas blanditiis cumque suscipit veniam! Dolore, ducimus magnam aperiam animi dolorem corrupti eius. Tempore, commodi ullam. Voluptatem consectetur ullam nesciunt impedit atque distinctio ut harum, error provident culpa ab amet maxime tempore enim pariatur esse facilis architecto iusto. Ut veritatis eius qui modi aut provident cum, illum sequi accusantium repudiandae enim ad sint iure, nisi rem nesciunt quam aliquid sit laudantium hic fuga doloribus neque reiciendis? Quia illum nam saepe ab, qui necessitatibus, dicta, suscipit a consectetur doloremque ipsa voluptatum dolore quisquam corporis accusamus molestiae tenetur? Sapiente tenetur, numquam aperiam doloribus quis dolore porro laborum expedita similique labore delectus quasi autem dolor amet magni impedit adipisci fugit eius facere odio perferendis. Temporibus quam numquam, in facere sequi optio corporis.</p>
+                        <h3 style="border-bottom: 1px solid rgb(212, 212, 212); padding-bottom: 5px; ">Actors</h3>
+                        <div class="actor">
+                            <div class="col-xs-11 col-md-10 col-centered">
+                                <div id="carousel" class="carousel slide" data-ride="carousel" data-type="multi" data-interval="2500">
+                                    <div class="carousel-inner">
+                                        <div class="item active">
+                                            <div class="carousel-col">
+                                                <div class="block red img-responsive"></div>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <div class="carousel-col">
+                                                <div class="block green img-responsive"></div>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <div class="carousel-col">
+                                                <div class="block blue img-responsive"></div>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <div class="carousel-col">
+                                                <div class="block yellow img-responsive"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                    
+                                    <!-- Controls -->
+                                    <div class="left carousel-control">
+                                        <a href="#carousel" role="button" data-slide="prev">
+                                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                    </div>
+                                    <div class="right carousel-control">
+                                        <a href="#carousel" role="button" data-slide="next">
+                                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                </div>
+                    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end left -->
+            <div class="col-lg-4">
+                <div class="sidebar p-3" style="border: 1px solid rgb(212, 212, 212); background-color: rgb(22, 22, 22)">
+                    <div class="toplists pb-3" style="color: white;">
+                        <h3 style="border-bottom: 1px solid rgb(212, 212, 212); padding-bottom: 5px; ">Top List</h3>
+                            <div class="card mb-3" style="max-width: 540px;">
+                                <div class="row g-0">
+                                  <div class="col-md-4">
+                                    <img src="/assets/img/avenger.jpg" style="min-height: 150px" class="img-fluid rounded-start" alt="...">
+                                  </div>
+                                  <div class="col-md-8">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Avenger End Game</h5>
+                                      <p style="font-size: 14px"><i class="bi bi-stopwatch"></i> 02 hours 30 minutes</p>
+                                      <p style="font-size: 15px">Comic, Magic</p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            <div class="card mb-3" style="max-width: 540px;">
+                                <div class="row g-0">
+                                  <div class="col-md-4">
+                                    <img src="/assets/img/avenger.jpg" style="min-height: 150px" class="img-fluid rounded-start" alt="...">
+                                  </div>
+                                  <div class="col-md-8">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Avenger End Game</h5>
+                                      <p style="font-size: 14px"><i class="bi bi-stopwatch"></i> 02 hours 30 minutes</p>
+                                      <p style="font-size: 15px">Comic, Magic</p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            <div class="card mb-3" style="max-width: 540px;">
+                                <div class="row g-0">
+                                  <div class="col-md-4">
+                                    <img src="/assets/img/avenger.jpg" style="min-height: 150px" class="img-fluid rounded-start" alt="...">
+                                  </div>
+                                  <div class="col-md-8">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Avenger End Game</h5>
+                                      <p style="font-size: 14px"><i class="bi bi-stopwatch"></i> 02 hours 30 minutes</p>
+                                      <p style="font-size: 15px">Comic, Magic</p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            <div class="card mb-3" style="max-width: 540px;">
+                                <div class="row g-0">
+                                  <div class="col-md-4">
+                                    <img src="/assets/img/avenger.jpg" style="min-height: 150px" class="img-fluid rounded-start" alt="...">
+                                  </div>
+                                  <div class="col-md-8">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Avenger End Game</h5>
+                                      <p style="font-size: 14px"><i class="bi bi-stopwatch"></i> 02 hours 30 minutes</p>
+                                      <p style="font-size: 15px">Comic, Magic</p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</section>
+
 @endsection
